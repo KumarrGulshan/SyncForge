349e09322686c92abf14f513f1bfed6f15dcad0d
Gulshan Kumar <gk6094397@gmail.com>
2026-03-10 06:25:58 +0000

    first commit

---
+++ src/main/java/com/syncforge/user/User.java
@@ -0,0 +1,32 @@
+package com.syncforge.user;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+    private Role role;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+}
